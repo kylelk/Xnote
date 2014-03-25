96f9d36b20b5e8bbf0e178f565f08407cb3cf798
kyle kersey <kylelk@comcast.net>
2014-03-25 15:48:03 -0700

    first commit

---
+++ src/com/example/xnote/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.xnote;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+
+/**
+ * @author kyle
+ * 
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		String baseDir = Environment.getExternalStorageDirectory()
+				.getAbsolutePath().toString()
+				+ "/xnote";
+		File folder = new File(baseDir);
+
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+
+
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+		if (Intent.ACTION_SEND.equals(action) && type != null)
+		{
+			handleSendText(intent); // Handle text being sent
+		}
+
+		Button mSaveButton = (Button) findViewById(R.id.save_note);
+		mSaveButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+
+				Toast.makeText(getApplicationContext(), "Saved Note", Toast.LENGTH_LONG).show();
+
+				EditText noteTitleFeild = (EditText) findViewById(R.id.note_title);
+				String mNoteTitle = noteTitleFeild.getText().toString();
+
+				EditText noteTextFeild = (EditText) findViewById(R.id.note_text);
+				String mNoteMessage = noteTextFeild.getText().toString();
+
+				SimpleDateFormat s = new SimpleDateFormat("ddMMyyyyhhmmss");
+				String mFormatedTimestamp = s.format(new Date());
+
+				try {
+
+					DocumentBuilderFactory docFactory = DocumentBuilderFactory
+							.newInstance();
+					DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+					// root elements
+					Document doc = docBuilder.newDocument();
+					Element rootElement = doc.createElement("note");
+					doc.appendChild(rootElement);
+
+					Element timestamp = doc.createElement("timestamp");
+					timestamp.appendChild(doc.createTextNode(mFormatedTimestamp));
+					rootElement.appendChild(timestamp);
+
+					Element title = doc.createElement("title");
+					title.appendChild(doc.createTextNode(mNoteTitle));
+					rootElement.appendChild(title);
+
+					Element message = doc.createElement("message");
+					message.appendChild(doc.createTextNode(mNoteMessage));
+					rootElement.appendChild(message);
+
+					long unixTime = System.currentTimeMillis() / 1000L;
+					String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/xnote/";
+					String fileNameTimestamp = baseDir+String.valueOf(unixTime)+".xml";
+
+					// write the content into xml file
+					TransformerFactory transformerFactory = TransformerFactory
+							.newInstance();
+					Transformer transformer = transformerFactory.newTransformer();
+					DOMSource source = new DOMSource(doc);
+					StreamResult result = new StreamResult(new File(fileNameTimestamp));
+
+
+					transformer.transform(source, result);
+
+					noteTitleFeild.setText("");
+					noteTextFeild.setText("");
+					noteTitleFeild.requestFocus();
+					//System.out.println("File saved!");
+
+				} catch (ParserConfigurationException pce) {
+					pce.printStackTrace();
+				} catch (TransformerException tfe) {
+					tfe.printStackTrace();
+				}
+
+			}
+		});
+
+	}
+	/**
+	 * @param intent
+	 */
+	public void handleSendText(Intent intent)
+	{
+		String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		if (sharedText != null)
+		{ 
+			EditText noteTextFeild = (EditText) findViewById(R.id.note_text);
+			noteTextFeild.setText(sharedText);
+		} 
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.recent_notes:
+            	Intent intent = new Intent();
+				intent.setClass(getApplicationContext(), RecentNotes.class);
+				startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
---
+++ src/com/example/xnote/RecentNotes.java
@@ -0,0 +1,160 @@
+package com.example.xnote;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public class RecentNotes extends Activity {
+	private ListView mainListView ;  
+	 private ArrayAdapter<String> listAdapter ;  
+	 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recent_notes);
+
+	    mainListView = (ListView) findViewById( R.id.recent_notes_list );  
+	    
+	    // Create and populate a List of planet names.  
+	    String[] planets = new String[] {};    
+	    ArrayList<String> planetList = new ArrayList<String>();  
+	    planetList.addAll( Arrays.asList(planets) );  
+	      
+	    // Create ArrayAdapter using the planet list.  
+	    listAdapter = new ArrayAdapter<String>(this, R.layout.list_view_row, planetList); 
+		
+		final String path = Environment.getExternalStorageDirectory().toString()+"/xnote/";
+		File f = new File(path);        
+		final File files[] = f.listFiles();	
+		final ArrayList<String> mNoteTitle = new ArrayList<String>();
+		final ArrayList<String> mNoteContent = new ArrayList<String>();
+		for (int i=0; i < files.length; i++)
+		{
+			//Log.d("Files", "FileName:" + file[i].getName());
+
+			try {
+				File fXmlFile = new File(path + files[i].getName());
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(fXmlFile);
+				doc.getDocumentElement().normalize();
+
+				listAdapter.add(doc.getElementsByTagName("title").item(0).getTextContent());
+				mNoteTitle.add(doc.getElementsByTagName("title").item(0).getTextContent());
+				mNoteContent.add(doc.getElementsByTagName("message").item(0).getTextContent());
+
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		mainListView.setAdapter( listAdapter ); 
+		mainListView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {                
+  
+                AlertDialog.Builder builder = new AlertDialog.Builder(RecentNotes.this);
+                builder.setTitle(mNoteTitle.get(position));
+                builder.setMessage(mNoteContent.get(position));
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Do something
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                builder.show();
+            }                   
+			
+        });
+		
+		mainListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                    final int position, long id) {                
+  
+                AlertDialog.Builder builder = new AlertDialog.Builder(RecentNotes.this);
+                builder.setTitle("Delete Note?");
+                builder.setMessage(mNoteTitle.get(position));
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Do something                    
+                    	File file = new File(path + files[position].getName());
+                    	file.delete();
+                        listAdapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                builder.show();
+                return true;
+            }
+		});
+
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.recent_notes_menu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.new_note_option:
+			Intent intent = new Intent();
+			intent.setClass(getApplicationContext(), MainActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
